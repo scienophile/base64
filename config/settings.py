50cf8204d6a4b1330a05e5b31849c5f1111f9fe6
scienophile <acuriousdreamer@tutanota.com>
2022-02-17 20:13:21 +0700

    add Procfile

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n&0en^a+ajusvxvk65t8x^869!ex65=fs$&a!gr8ug-q+y&u4-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('base64/', include('bin2text.urls')),
+    path('', include('bin2text.urls')),
     path('admin/', admin.site.urls),
 ]
